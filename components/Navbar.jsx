be322d115e50beda8520faf7501d15217f73a73a
Looch Mane <loochmane4@gmail.com>
2026-01-03 04:01:22 +0000

    add a cart icon (_for element <Navbar>_)

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 
 const Navbar = () => {
 
-  const { isSeller, router } = useAppContext();
+  const { isSeller, router, getCartCount } = useAppContext();
 
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700">
@@ -35,19 +35,26 @@ const Navbar = () => {
 
       </div>
 
-      <ul className="hidden md:flex items-center gap-4 ">
-        <Image className="w-4 h-4" src={assets.search_icon} alt="search icon" />
+      <ul className="hidden md:flex items-center gap-6 ">
+        <Image className="w-4 h-4 cursor-pointer" src={assets.search_icon} alt="search icon" />
+        <Link href={'/cart'} className='relative'>
+          <Image className="w-5 h-5" src={assets.cart_icon} alt="cart icon" />
+          {getCartCount() > 0 && <div className='absolute -top-2 -right-2 bg-orange-600 text-white text-xs w-4 h-4 flex items-center justify-center rounded-full'>{getCartCount()}</div>}
+        </Link>
         <button className="flex items-center gap-2 hover:text-gray-900 transition">
           <Image src={assets.user_icon} alt="user icon" />
           Account
         </button>
       </ul>
 
-      <div className="flex items-center md:hidden gap-3">
+      <div className="flex items-center md:hidden gap-5">
         {isSeller && <button onClick={() => router.push('/seller')} className="text-xs border px-4 py-1.5 rounded-full">Seller Dashboard</button>}
+        <Link href={'/cart'} className='relative'>
+          <Image className="w-5 h-5" src={assets.cart_icon} alt="cart icon" />
+          {getCartCount() > 0 && <div className='absolute -top-2 -right-2 bg-orange-600 text-white text-xs w-4 h-4 flex items-center justify-center rounded-full'>{getCartCount()}</div>}
+        </Link>
         <button className="flex items-center gap-2 hover:text-gray-900 transition">
           <Image src={assets.user_icon} alt="user icon" />
-          Account
         </button>
       </div>
     </nav>
