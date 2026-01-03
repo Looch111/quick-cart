b8746b0d3fb712cec07d715adb9646ad438e57f6
Looch Mane <loochmane4@gmail.com>
2026-01-03 03:52:30 +0000

    rounded full (_for element button_)

--- src/app/admin/users/page.tsx
+++
@@ -1,5 +0,0 @@
-import UsersView from "@/components/admin/users-view";
-
-export default function AdminUsersPage() {
-  return <UsersView />;
-}
--- src/components/dashboard/dashboard.tsx
+++
@@ -1,6 +0,0 @@
-// This component has been refactored into a layout and page-specific views.
-// See src/components/dashboard/dashboard-layout.tsx
-// This file will be removed in a future update.
-export default function Dashboard() {
-  return null;
-}
