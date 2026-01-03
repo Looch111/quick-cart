aab50d1ac1b3cc1b9188dc29c400385303b4d7c7
Looch Mane <loochmane4@gmail.com>
2026-01-03 15:35:21 +0000

    Try fixing this error: `Build Error: Failed to compile. Error source: ./

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -15,10 +15,10 @@ export const firebaseConfig = {
 
 // IMPORTANT: REPLACE WITH YOUR FIREBASE CONFIGURATION
 export const firebaseConfig = {
-  apiKey: "REPLACE_WITH_YOUR_API_KEY",
-  authDomain: "REPLACE_WITH_YOUR_AUTH_DOMAIN",
-  projectId: "REPLACE_WITH_YOUR_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_YOUR_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_WITH_YOUR_MESSAGING_SENDER_ID",
-  appId: "REPLACE_WITH_YOUR_APP_ID"
-};
+  apiKey: "AIzaSyDlbeO-lWkgVHAckFfNuFr6h8vjFZUMG70",
+  authDomain: "login-and-sign-up-e39e4.firebaseapp.com",
+  projectId: "login-and-sign-up-e39e4",
+  storageBucket: "login-and-sign-up-e39e4.firebasestorage.app",
+  messagingSenderId: "1025789251454",
+  appId: "1:1025789251454:web:ffe54a3db2f480737b6d2f"
+};;
