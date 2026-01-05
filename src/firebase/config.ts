94d60004f379bc1fb903ea15e3506e73bf2bbedc
Looch Mane <loochmane4@gmail.com>
2026-01-05 17:16:32 +0000

    coonect the app to work and things change realtime

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,4 @@
+
 // This file is used to store the Firebase configuration
 // You should replace this with your own Firebase configuration
 // For example:
@@ -18,7 +19,8 @@ export const firebaseConfig = {
   apiKey: "AIzaSyDlbeO-lWkgVHAckFfNuFr6h8vjFZUMG70",
   authDomain: "login-and-sign-up-e39e4.firebaseapp.com",
   projectId: "login-and-sign-up-e39e4",
-  storageBucket: "login-and-sign-up-e39e4.firebasestorage.app",
+  storageBucket: "login-and-sign-up-e39e4.appspot.com",
   messagingSenderId: "1025789251454",
   appId: "1:1025789251454:web:ffe54a3db2f480737b6d2f"
-};;
+};
+
