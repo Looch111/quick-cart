954d26c6f971e2ded38b5031c8476f1c2fbc7631
Looch Mane <loochmane4@gmail.com>
2026-01-06 17:34:23 +0000

    thi is th code 'use client' import React from "react"; import { assets }

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { assets } from "@/assets/assets";
 import OrderSummary from "@/components/OrderSummary";
 import Image from "next/image";
