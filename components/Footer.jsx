2d61a9fdfcde77c31bde2000ef01f7e371a7954b
Looch Mane <loochmane4@gmail.com>
2026-01-15 01:16:48 +0000

    change QuickCart to EUI Tap&Shop (_for element p_)

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <div className="w-4/5">
           <Image src={assets.logo} alt="logo" width={170} height={45} className="w-32 md:w-[170px]" />
           <p className="mt-6 text-sm">
-            QuickCart is your one-stop shop for the latest and greatest in electronics, gadgets, and accessories. We are dedicated to providing you with a seamless shopping experience, from browsing to delivery.
+            EUI Tap&Shop is your one-stop shop for the latest and greatest in electronics, gadgets, and accessories. We are dedicated to providing you with a seamless shopping experience, from browsing to delivery.
           </p>
         </div>
 
